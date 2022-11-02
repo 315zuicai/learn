164cf3c1eb552649fc488ec5f61ece35b105141c
dingkaiqiang <dingkaiqiang@baidu.com>
2022-11-02 15:43:24 +0800

    first commit

---
+++ myPromise.js
@@ -0,0 +1,62 @@
+class myPromise {
+    static PENDING = 'pending';
+    static FULFILLED = 'fulfilled';
+    static REJECTED = 'rejected';
+    constructor(func) {
+        this.PromiseState = myPromise.PENDING;
+        this.PromiseResult = null;
+        this.onFulfilledCallbacks = []; // 保存成功回调
+        this.onRejectedCallbacks = []; // 保存失败回调
+        try {
+            func(this.resolve.bind(this), this.reject.bind(this));
+        } catch (error) {
+            this.reject(error)
+        }
+    }
+    resolve(result) {
+        if (this.PromiseState === myPromise.PENDING) {
+            this.PromiseState = myPromise.FULFILLED;
+            this.PromiseResult = result;
+            this.onFulfilledCallbacks.forEach(callback => {
+                callback(result)
+            })
+        }
+    }
+    reject(reason) {
+        if (this.PromiseState === myPromise.PENDING) {
+            this.PromiseState = myPromise.REJECTED;
+            this.PromiseResult = reason;
+            this.onRejectedCallbacks.forEach(callback => {
+                callback(reason)
+            })
+        }
+    }
+    then(onFulfilled, onRejected) {
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+        onRejected = typeof onRejected === 'function' ? onRejected : reason => {
+            throw reason;
+        };
+        if (this.PromiseState === myPromise.PENDING) {
+            this.onFulfilledCallbacks.push(() => {
+                setTimeout(() => {
+                    onFulfilled(this.PromiseResult);
+                });
+            });
+            this.onRejectedCallbacks.push(() => {
+                setTimeout(() => {
+                    onRejected(this.PromiseResult);
+                });
+            });
+        }
+        if (this.PromiseState === myPromise.FULFILLED) {
+            setTimeout(() => {
+                onFulfilled(this.PromiseResult);
+            });
+        }
+        if (this.PromiseState === myPromise.REJECTED) {
+            setTimeout(() => {
+                onRejected(this.PromiseResult);
+            });
+        }
+    }
+}
